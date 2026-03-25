96330acc7d6bbe76747addda8f5be2e14881ce23
Tijani Salami O. <salamitijani02@gmail.com>
2026-03-25 11:16:00 +0100

    Restrict CORS to localhost:3000

--- 3kstBackend/src/main/java/com/_3kstbackend/config/SecurityConfig.java
+++ 3kstBackend/src/main/java/com/_3kstbackend/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         System.out.println("Configuring CORS...");
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
